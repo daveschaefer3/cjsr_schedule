245f5494a91fc73fcc6f880d7f069f1a1470e227
Dave Schaefer <daveschaefer3@gmail.com>
2013-09-19 15:36:37 -0600

    Initial commit. Handlebars templates are filled with json test data.

---
+++ javascripts/showJson.js
@@ -0,0 +1,466 @@
+/*
+  "shows": [
+    ,
+    {
+      "id": ,
+      "name": "",
+      "day": "",
+      "start": ,
+      "end": ,
+      "tagline": "",
+      "hosts": [
+        {"name": ""},
+        {"name": ""}
+      ],
+      "description": "",
+      "email": "",
+      "twitter": "",
+      "facebook": "",
+      "website": ""
+    }
+  ]
+*/
+
+var showJson = {
+  "shows": [
+    {
+      "id": 1,
+      "name": "Musica Nova",
+      "day": "sat",
+      "start": 900,
+      "end": 1100,
+      "tagline": "Musica Nova - Classical Music",
+      "hosts": [
+        {"name": "Robin Hebert"}
+      ],
+      "description": "Musica Nova takes a journey of discovery through one thousand years of classical music history. Canadian composers are featured regularly along with occasional surprises of jazz and world music.",
+      "email": "musicanova@cjsr.com"
+    },
+    {
+      "id": 2,
+      "name": "Northern Air",
+      "day": "sun",
+      "start": 1600,
+      "end": 1700,
+      "tagline": "Northern Air showcases new folk and folk influenced music from all across Canada.",
+      "hosts": [
+        {"name": "Kristi Beckman"}
+      ],
+      "description": "Northern Air features exclusively Canadian folk and folk influenced music released within the last year. Focused on sharing new interesting Canadian folk music and providing information on songs and artists. Special focus each show on local talent from CJSRs home of Alberta.",
+      "email": "kristi@cjsr.com",
+      "website": "http://northernair.tumblr.com"
+    },
+    {
+      "id": 3,
+      "name": "Hip Hop Buffet",
+      "day": "sat",
+      "start": 100,
+      "end": 300,
+      "tagline": "Hip Hop Buffet - Your one stop shop for good hip hop.",
+      "hosts": [
+        {"name": "Butter Chicken Wolf"},
+        {"name": "Cartwright The Architect"},
+        {"name": "DJ Double Booked"}
+      ],
+      "description": "Playing hip hop music from the old school, new school and everything in between.",
+      "email": "music@hiphopbuffet.ca",
+      "twitter": "@hiphopbuffet",
+      "facebook": "http://www.facebook.com/hiphopbuffet",
+      "website": "http://www.hiphopbuffet.ca"
+    },
+    {
+      "id": 4,
+      "name": "VRSS",
+      "day": "mon",
+      "start": 1000,
+      "end": 1100,
+      "tagline": "An eclectic music showdown.",
+      "hosts": [
+        {"name": "Matt and Annie"},
+      ],
+      "description": "",
+      "email": "vrss@cjsr.com",
+      "twitter": "@vrsscjsr",
+      "website": "http://vrss.tumblr.com"
+    },
+    {
+      "id": 5,
+      "name": "Ragas and Rhythms",
+      "day": "sun",
+      "start": 1200,
+      "end": 1300,
+      "tagline": "Music from the Indo/Pakistani region.",
+      "hosts": [
+        {"name": "Janine Pidruchney"},
+        {"name": "Asif Nazarali"}
+      ],
+      "description": "All genres of music from India and Pakistan: Classical, popular, devotional, fusion, folk, Bollywood, jazz, blues, electronic.",
+      "email": "asifnazarali@gmail.com",
+      "website": "N/A"
+    },
+    {
+      "id": 6,
+      "name": "Twang n' Thangs",
+      "day": "mon",
+      "start": 1300,
+      "end": 1500,
+      "tagline": "The best in independent Country/Roots/Folk and Old Timey Gems.",
+      "hosts": [
+        {"name": "Thea and Amy"}
+      ],
+      "description": "",
+      "email": "twang@cjsr.com",
+      "website": "http://www.twangnthangscjsr.wordpress.com"
+    },
+    {
+      "id": 7,
+      "name": "Full English Breakfast",
+      "day": "thurs",
+      "start": 700,
+      "end": 900,
+      "tagline": "Eclectic music, current news and bikey chat.",
+      "hosts": [
+        {"name": "Peter Chapman with CJSR's bicycle traffic reporter Karly Coleman"}
+      ],
+      "description": "A lively blend of new music, quirky news and bicycle chat. On the last Thursday of the month, join us from 8am for an extended topic on Bike Bits.",
+      "email": "peter@cjsr.com",
+      "twitter": "@petercjsr",
+      "facebook": "http://www.facebook.com/ThePeterChapman",
+      "website": "N/A",
+      "othersocialmedia": [
+        {"url": "http://www.mixcloud.com/petercjsr/"},
+        {"url": "http://www.soundcloud.com/ptrchpmn"},
+        {"url": "http://www.myspace.com/feboncjsr"}
+      ]
+    },
+    {
+      "id": 8,
+      "name": "Hip Hop Spread on Planet Bread",
+      "day": "sun",
+      "start": 2200,
+      "end": 2300,
+      "tagline": "CJSR's only global hip hop music show, tracking the globalization of rap.",
+      "hosts": [
+        {"name": "DJ Spooky Luke"},
+        {"name": "Tam Tam the Destroyer"}
+      ],
+      "description": "Hip Hop Spread on Plant Bread investigates the global spread of hip hop culture. Tracking the spread of hip hop worldwide from the 1980's until the most recent global rap.",
+      "email": "blacksparrows@hotmail.com",
+      "website": "http://radioscum.blogspot.com"
+    },
+    {
+      "id": 9,
+      "name": "Terra Informa",
+      "day": "tues",
+      "start": 1700,
+      "end": 1730,
+      "tagline": "Terra Informa - Your source for environmental news.",
+      "hosts": [
+        {"name": "The Terra Informa Team"}
+      ],
+      "description": "Each week Terra Informa delivers coverage of breaking environmental news from across the country and around the world. From corporate wrongdoings to tips on living a little greener, we're your source for the news that the mainstream media missed.",
+      "email": "terra@cjsr.com",
+      "twitter": "@Terra_Informa",
+      "facebook": "http://www.facebook.com/terrainforma",
+      "website": "http://www.terrainforma.ca",
+      "othersocialmedia": [
+        {"url": "http://itunes.apple.com/ca/podcast/terra-informa/id480264280"}
+      ]
+    },
+    {
+      "id": 10,
+      "name": "Skeptically Speaking",
+      "day": "fri",
+      "start": 1800,
+      "end": 1900,
+      "tagline": "Guiding you through the fascinating world of science and critical thinking.",
+      "hosts": [
+        {"name": "Desiree Schell"}
+      ],
+      "description": "Skeptically Speaking interviews the experts to help listeners understand the arguments and evidence behind what’s in the news and on the shelves, and delves into the slippery social issues to which one can apply science and critical thinking.",
+      "email": "feedback@skepticallyspeaking.com",
+      "twitter": "@SkepticalRadio",
+      "facebook": "https://www.facebook.com/SkepticallySpeaking",
+      "website": "http://skepticallyspeaking.com/"
+    },
+    {
+      "id": 11,
+      "name": "The Missing Link",
+      "day": "unknown",
+      "start": 2300,
+      "end": 2400,
+      "tagline": "Hip Hop they've been hiding from you and your loved ones.",
+      "hosts": [
+        {"name": "DJ Dirty Needlz"},
+        {"name": "DJ Dice"}
+      ],
+      "description": "Underground, independent and Canadian Hip Hop from the eighties until now.",
+      "email": "themissinglinkcjsr@gmail.com",
+      "twitter": "@touch182",
+      "website": "N/A"
+    },
+    {
+      "id": 12,
+      "name": "Ranking B's Fortress of Solitude",
+      "day": "tues",
+      "start": 1800,
+      "end": 1900,
+      "tagline": "Soul-centric Sounds from Then &amp; Now",
+      "hosts": [
+        {"name": "Ranking B"}
+      ],
+      "description": "1 hour of Soul, Funk, Afrobeat, R'n'B, Boogaloo, Pysch, & Neo-Soul.",
+      "email": "rankingb@cjsr.com",
+      "twitter": "@rankingb",
+      "website": "http://www.fortressofsolitude.blogspot.ca"
+    },
+    {
+      "id": 13,
+      "name": "Se&ntilde;or Blues",
+      "day": "sat",
+      "start": 700,
+      "end": 900,
+      "tagline": "Blues music programming",
+      "hosts": [
+        {"name": "Qasim Hirani"}
+      ],
+      "description": "Two hours of nothin' but the blues! Tune in to hear blues classics, new releases, buried treasures, artist spotlights, and of course, live blues!",
+      "email": "senorblues@cjsr.com",
+      "website": "http://senorbluesblog.blogspot.ca"
+    },
+    {
+      "id": 14,
+      "name": "Field Tracker",
+      "day": "sun",
+      "start": 2300,
+      "end": 2400,
+      "tagline": "Ambient music radio.",
+      "hosts": [
+        {"name": "Matthew Gooding"}
+      ],
+      "description": "An all ambient radio show focusing on everything that is slow. Drone, electronic, and experimental music at the sleepiest time of the week.",
+      "email": "fieldtracker@cjsr.com",
+      "facebook": "https://www.facebook.com/FieldTracker",
+      "website": "http://fieldtracker.wordpress.com"
+    },
+    {
+      "id": 15,
+      "name": "Catch The Beat",
+      "day": "sat",
+      "start": 1600,
+      "end": 1800,
+      "tagline": "Eclectic electronic dance music program.",
+      "hosts": [
+        {"name": "Bron, Raebot & Moe Lowe"}
+      ],
+      "description": "Canada's longest running electronic music program showcasing the best in eclectic underground dance music from past, present and future.  First aired in September 1986 it has become a backbone to the underground dance music scene in Edmonton. ",
+      "email": "danksoul@gmail.com",
+      "facebook": "https://www.facebook.com/groups/117941984887958/",
+      "website": "http://catch-the-beat.blogspot.ca/",
+      "othersocialmedia": [
+        {"url": "http://www.mixcloud.com/CatchTheBeat"}
+      ]
+    },
+    {
+      "id": 16,
+      "name": "Gaywire",
+      "day": "thurs",
+      "start": 1800,
+      "end": 1900,
+      "tagline": "Gaywire - Radical queer news and culture radio.",
+      "hosts": [
+        {"name": "Alexa DeGagne"},
+        {"name": "Jil Jorgensen"},
+        {"name": "Diana Olexin"}
+      ],
+      "description": "Since the 1980s Gaywire has been Edmonton's source for queer news, politics, arts, culture, and beyond.",
+      "email": "gaywire@cjsr.com",
+      "twitter": "@gaywire",
+      "facebook": "https://www.facebook.com/gaywire",
+      "website": "http://gaywire.tumblr.com"
+    },
+    {
+      "id": 17,
+      "name": "Local-Motive",
+      "day": "mon",
+      "start": 900,
+      "end": 1000,
+      "tagline": "Local-Motive - Eclectic programming to your local dial.",
+      "hosts": [
+        {"name": "Mark Berg"},
+        {"name": "Amanda Khamvongsa"}
+      ],
+      "description": "Local-Motive brings new music each week to your local dial.",
+      "email": "localmotive@cjsr.com",
+      "twitter": "@RomancingVinyl",
+      "website": "http://www.romancingvinyl.com"
+    },
+    {
+      "id": 18,
+      "name": "The Laughing Face",
+      "day": "tues",
+      "start": 1300,
+      "end": 1400,
+      "tagline": "Themed eclectic music show",
+      "hosts": [
+        {"name": "Nancy"}
+      ],
+      "description": "The Laughing Face is an hour-long eclectic music show with a different theme every week. The music in a particular episode can come from different genres, time periods, or countries, but it all relates somehow to the theme, whether that theme is Star Trek, musicals, winter, or George Orwell. Tune in for music, geekery, and fun!",
+      "email": "thelaughingface@cjsr.com",
+      "twitter": "@thelaughingface",
+      "website": "http://thelaughingface.tumblr.com"
+    },
+    {
+      "id": 19,
+      "name": "Afro Caribbean Beats",
+      "day": "sun",
+      "start": 1400,
+      "end": 1500,
+      "tagline": "Contemporary African and Caribbean music",
+      "hosts": [
+        {"name": "DJ Pozo (Abofu Alemka)"}
+      ],
+      "description": "",
+      "email": "djpozo@cjsr.com",
+      "website": "N/A"
+    },
+    {
+      "id": 20,
+      "name": "Jazzology",
+      "day": "sun",
+      "start": 1100,
+      "end": 1300,
+      "tagline": "Jazz show exploring new artists and old chestnuts.",
+      "hosts": [
+        {"name": "Steve MacLeod"}
+      ],
+      "description": "Fantastic Jazz journey for those new to Jazz or a passionate fan.",
+      "email": "jazzology@shaw.ca",
+      "website": "N/A"
+    },
+    {
+      "id": 21,
+      "name": "Soul of the City",
+      "day": "thurs",
+      "start": 1200,
+      "end": 1300,
+      "tagline": "Looking into the arts & culture that feed the soul of the city.",
+      "hosts": [
+        {"name": "Mark Rodgers"}
+      ],
+      "description": "",
+      "email": "",
+      "twitter": "@MRsoulofthecity",
+      "website": "N/A"
+    },
+    {
+      "id": 22,
+      "name": "Voice of Eriterea",
+      "day": "sun",
+      "start": 1300,
+      "end": 1400,
+      "tagline": "Offers news, community event announcements,  educational materials (on democracy, governance, …), cultural songs.",
+      "hosts": [
+        {"name": "Mike Berhane"},
+        {"name": "Zed Ghebremusse"},
+        {"name": "Habteab Tecle"}
+      ],
+      "description": "The program is shared by two Eritrean entities – one by the Eritrean Community Association of Edmonton (ECAE) and the other by a group that works as the mouth piece of the totalitarian regime Eritrea.  The one covered by the ECAE focuses more on the life situation of Eritreans in Edmonton/diaspora.  It is the voice of reason and democracy for all Eritreans.",
+      "email": "mebrahtu@telus.net",
+      "website": "N/A"
+    },
+    {
+      "id": 23,
+      "name": "Necrotechs UNITE!",
+      "day": "tues",
+      "start": 2100,
+      "end": 2300,
+      "tagline": "Necrotechs UNITE!: Industrial/Metal music on CJSR",
+      "hosts": [
+        {"name": "The Shadow"}
+      ],
+      "description": "Bringing together listeners of all dark natures! A mix of electronic-based and dark music, including industrial, electro-club, techno, heavy metal, nu metal, psychobilly and other fun tunes. It's all music I enjoy and want to share with the listeners!",
+      "email": "shadow@cjsr.com",
+      "twitter": "@sulkingShadow",
+      "website": "http://www.fuddafudda.com",
+      "othersocialmedia": [
+        {"url": "http://www.myspace.com/sulkingshadow"}
+      ]
+    },
+    {
+      "id": 24,
+      "name": "Fourre-Tout",
+      "day": "tues",
+      "start": 1100,
+      "end": 1300,
+      "tagline": "Fourre-Tout a French suitcase, a variety show in which all francophone can belong.",
+      "hosts": [
+        {"name": "Chantal Londji Dang"},
+        {"name": ""}
+      ],
+      "description": "We are a magazine radio show that aims at informing, entertaining and uniting francophones while connecting with the various cultures that form our Francophone/Francophile Edmonton Community. The programming of Fourre-Tout is committed to reflect the particular characteristics of French cultural mosaic.",
+      "email": "fourre-tout@shaw.ca",
+      "twitter": "@fourre_tout",
+      "facebook": "https://www.facebook.com/pages/Fourre-Tout/239144247826",
+      "website": "http://www.myspace.com/fourretout"
+    },
+    {
+      "id": 25,
+      "name": "The Tiny Bubbles Radio Hour",
+      "day": "tues",
+      "start": 2000,
+      "end": 2100,
+      "tagline": "Radio that will change your life. Missing it would be foolish.",
+      "hosts": [
+        {"name": "Heather"}
+      ],
+      "description": "Show tunes, old comedy records, rock 'n roll, pop, The Omnivore's Guide to the Arts w/ Lada, and the Gmail Agony Line.",
+      "email": "tinybubblesradio@gmail.com",
+      "twitter": "@bubblesradio",
+      "website": "http://tinybubblesradio.tumblr.com"
+    },
+    {
+      "id": 26,
+      "name": "Acimowin",
+      "day": "fri",
+      "start": 900,
+      "end": 1100,
+      "tagline": "'IndiginiZing the Airwaves' with 'jo-jo on the Radioh', ACIMOWIN is Cree for let’s talk, let’s share, let’s tell stories and that is just what we do.",
+      "hosts": [
+        {"name": "Jo-jo"}
+      ],
+      "description": "'IndigeniZing the AirwaveZ' is a mixed media program of video and radio.  Which brings audiences behind the scene, up close and personal as host, Jo-Jo invites guests into a forum for discussion on indignity and current issues we face as indigenous people.  The show also highlights and celebrates our gifts and talents as indigenous artists, musicians, performers and entertainers.",
+      "email": "acimowin@cjsr.com",
+      "twitter": "@acimowin",
+      "website": "N/A"
+    },
+    {
+      "id": 27,
+      "name": "Sounds Ukranian",
+      "day": "fri",
+      "start": 1900,
+      "end": 2100,
+      "tagline": "Not your parents' Ukrainian music show.",
+      "hosts": [
+        {"name": "Lada"}
+      ],
+      "description": "Ukrainian music programming covering at least six centuries and even more genres, with new tunes every week.",
+      "email": "soundsukrainian@cjsr.com",
+      "website": "N/A"
+    },
+    {
+      "id": 28,
+      "name": "5 River Beat",
+      "day": "thurs",
+      "start": 2000,
+      "end": 2100,
+      "tagline": "5 River Beat – CJSR’s Bhangra Specialist",
+      "hosts": [
+        {"name": "Ramneek"}
+      ],
+      "description": "Raw Punjabi Folk, Bhangra and Bollywood Funk Straight from the Motherland.",
+      "email": "5riverbeat@cjsr.com",
+      "website": "http://5riverbeat.com"
+    }
+  ]
+};
