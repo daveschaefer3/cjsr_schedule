633ed10e50f2094e68c448419aa8637e0847406a
Dave Schaefer <daveschaefer3@gmail.com>
2014-01-09 20:21:31 -0700

    Style changes and debugging ^ schedule.less - color changes ^ schedule.js - commented autoSizeText to fix crashing on certain browsers

--- javascripts/schedule.js
+++ javascripts/schedule.js
@@ -221,32 +221,32 @@ $(function(){
     $('#bbcBanner').height(58);
   }; // end of setShowHeights
 
-  var autoSizeText = function(theClass) {
-    var el, elements, _i, _len, _results;
-    elements = $('.' + theClass);
-    //console.log(elements);
-    if (elements.length < 0) {
-      return;
-    }
-    _results = [];
-    for (_i = 0, _len = elements.length; _i < _len; _i++) {
-      el = elements[_i];
-      _results.push((function(el) {
-        var resizeText, _results1;
-        resizeText = function() {
-          var elNewFontSize;
-          elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
-          return $(el).css('font-size', elNewFontSize);
-        };
-        _results1 = [];
-        while (el.scrollHeight > el.offsetHeight) {
-          _results1.push(resizeText());
-        }
-        return _results1;
-      })(el));
-    }
-    return _results;
-  }; // end of autoSizeText
+  // var autoSizeText = function(theClass) {
+  //   var el, elements, _i, _len, _results;
+  //   elements = $('.' + theClass);
+  //   //console.log(elements);
+  //   if (elements.length < 0) {
+  //     return;
+  //   }
+  //   _results = [];
+  //   for (_i = 0, _len = elements.length; _i < _len; _i++) {
+  //     el = elements[_i];
+  //     _results.push((function(el) {
+  //       var resizeText, _results1;
+  //       resizeText = function() {
+  //         var elNewFontSize;
+  //         elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+  //         return $(el).css('font-size', elNewFontSize);
+  //       };
+  //       _results1 = [];
+  //       while (el.scrollHeight > el.offsetHeight) {
+  //         _results1.push(resizeText());
+  //       }
+  //       return _results1;
+  //     })(el));
+  //   }
+  //   return _results;
+  // }; // end of autoSizeText
 
   var eventListeners = function() {
     // fill modal with the featured show.
@@ -363,7 +363,7 @@ $(function(){
 
   positionShowDivs();
   setShowHeights();
-  autoSizeText('schedShow');
+  // autoSizeText('schedShow');
 
   // template legend
   $('#scheduleBox').append(clearSource);
